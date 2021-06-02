8367b89f713546d0c47d08e6ffce656e06eeb3b1
Ashish <ashu8006kumar@gmail.com>
2021-06-02 21:14:00 +0530

    Day 8: email send code

--- src/main/java/io/arha/ticketsvc/controller/HomeController.java
+++ src/main/java/io/arha/ticketsvc/controller/HomeController.java
@@ -3,6 +3,9 @@ package io.arha.ticketsvc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,28 @@ public class HomeController {
 
 	@GetMapping("")
 	public String welcome() {  
+		
+		JavaMailSenderImpl javaMailSender= new JavaMailSenderImpl(); 
+		javaMailSender.setHost(emailProps.getHost());
+		javaMailSender.setPort(emailProps.getPort());
+		javaMailSender.setUsername(emailProps.getUsername());
+		javaMailSender.setPassword(emailProps.getPassword());
+		
+		
+		///
+		SimpleMailMessage message= new SimpleMailMessage();
+		message.setTo("ashu8006kumar@gmail.com");
+		message.setFrom("tickectingapp@gmail.com");
+		message.setSubject("dyummy message ");
+		message.setText("Hi user,\nthis is a dummy message.");
+		
+		
+		javaMailSender.send(message);
+		
+		
+		
+		
+		
 		return emailProps.getHost();
 				
 	}
