c73347cc708b88dc53fbb0fec1a74a526a171f04
Ashish <ashu8006kumar@gmail.com>
2021-06-11 21:36:00 +0530

    forgot passsword link

--- src/main/java/io/arha/ticketsvc/controller/RegisterController.java
+++ src/main/java/io/arha/ticketsvc/controller/RegisterController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.arha.ticketsvc.dto.ForgotPasswordDto;
 import io.arha.ticketsvc.dto.RegisterDto;
+import io.arha.ticketsvc.repository.UserRepository;
 import io.arha.ticketsvc.service.RegisterServcie;
 
 @RestController
@@ -19,15 +21,30 @@ public class RegisterController {
 	@Autowired
 	private RegisterServcie registerServcie;
 
+	@Autowired // TODO remove it on MONDAY
+	private UserRepository userRepository;
+
 	@PostMapping("")
 	public void registerNewUser(@Valid @RequestBody RegisterDto register) {
 		registerServcie.registerNewUser(register);
-		
+
 	}
-	
+
 	@GetMapping("/verify/{registerLinkId}")
 	public void verify(@PathVariable String registerLinkId) {
 		registerServcie.findAndVerifyUserByLinkId(registerLinkId);
-		
+
+	}
+
+	@GetMapping("/forgot/password")
+	public void forgotPassword(@Valid @RequestBody ForgotPasswordDto forgotPasswordDto) {
+		if(!forgotPasswordDto.getPassword().equals(forgotPasswordDto.getConfirmPassword())) {
+			throw new RuntimeException("Password not matching with confirm password.");
+		}
+		if(userRepository.countByUsername(forgotPasswordDto.getUsername())>0) {
+			
+		}else {
+			throw new RuntimeException("User not fond.");
+		} 
 	}
 }
